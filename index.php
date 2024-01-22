2d8d2ca75d2a5d105b6e429960d97d557f82e915
alinakopasava <s27421@pjwstk.edu.pl>
2024-01-22 13:27:29 +0100

    Dodanie strony glownej

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>QuizWhiz</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<header>
+        <div class="header-left">
+            <h1 class="text-header">QuizWhiz</h1><br><br><br>
+        </div>
+        <div class="header-right">
+            <a href="login.php" class="but1">Log in</a>
+            <a href="registration.php" class="but1">Sign up</a>
+        </div>
+    </header>
+
+<div class="container">
+    <h1 class="head">Embrace the Quizvolution: Test Your Wits!</h1>
+    <p class="head">Engage your intellect and challenge your knowledge with our exciting quiz!<br>
+        From trivia to brain-teasers, our quiz offers an entertaining experience <br>that will keep you coming back for more.<br>
+    </p>
+        <p class="head">Join us and embark on a quest for knowledge! </p>
+    <div class="button-container">
+    <a href="registration.php" class="but1">Get Started</a>
+        </div>
+
+
+</div>
+
+</body>
+</html>
